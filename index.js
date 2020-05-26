e5a35de1b157e40ff208df99defc0539a5d891f9
saranya01-saro <64539395+saranya01-saro@users.noreply.github.com>
2020-05-26 15:42:14 +0530

    add files via

---
+++ index.js
@@ -0,0 +1,42 @@
+alert("s");
+var model = undefined;
+const classifierElement = document.getElementById('classifier');
+const loaderElement = document.getElementById('loader');
+
+async function initialize() {
+
+    model = await tf.loadLayersModel('tensor/model.json');
+    classifierElement.style.display = 'block';
+    loaderElement.style.display = 'none';
+
+    document.getElementById('predict').addEventListener('click', () => predict());
+
+}
+
+async function predict () {
+
+    const imageElement = document.getElementById('img');
+    let tensorImg = tf.browser.fromPixels(imageElement).resizeNearestNeighbor([64, 64]).toFloat().expandDims();
+    prediction = await model.predict(tensorImg).data();
+
+    if (prediction[0] == 0) {
+
+        alert("You uploaded a cat!");
+
+    } else if (prediction[0] == 1) {
+
+        alert("You uploaded a dog!");
+
+    } else {
+        alert("Hummm... a weird error occurred.");
+    }
+
+}
+
+function changeImage() {
+    var imageDisplay = document.getElementById('img');
+    var uploadedImage = document.getElementById('my-file-selector').files[0];
+    imageDisplay.src = URL.createObjectURL(uploadedImage);
+}
+
+initialize();
